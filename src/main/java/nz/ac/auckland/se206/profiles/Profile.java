42e1356519e61dce0c63a967ef150f2c8de2f57c
Guryash <gmat224@aucklanduni.ac.nz>
2022-09-16 10:33:30 +1200

    Added tracking features for updating wins, losses and setting fastest time

--- src/main/java/nz/ac/auckland/se206/profiles/Profile.java
+++ src/main/java/nz/ac/auckland/se206/profiles/Profile.java
@@ -1,8 +1,5 @@
 package nz.ac.auckland.se206.profiles;
 
-import com.opencsv.CSVReader;
-import com.opencsv.CSVWriter;
-import com.opencsv.exceptions.CsvException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -11,121 +8,136 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
+
 public class Profile {
 
-  private String username;
+	private String username;
 
-  private int wins;
-  private int losses;
-  private List<String> wordHistory;
-  private int fastestWinTime;
+	private int wins;
+	private int losses;
+	private List<String> wordHistory;
+	private int fastestWinTime;
 
-  private File csvFile;
-  private List<String[]> csvList;
+	private File csvFile;
+	private List<String[]> csvList;
 
-  public Profile(String username) throws IOException, CsvException {
-    this.username = username.toLowerCase();
+	public Profile(String username) throws IOException, CsvException {
+		this.username = username.toLowerCase();
 
-    // location of file
-    String filePath = "src/main/resources/profiles/" + this.username + ".csv";
-    this.csvFile = new File(filePath);
+		// location of file
+		String filePath = "src/main/resources/profiles/" + this.username + ".csv";
+		this.csvFile = new File(filePath);
 
-    if (!this.csvFile.isFile()) {
-      // create {username}.csv file with default values
-      try {
-        // create a new csv file writer
-        FileWriter outputfile = new FileWriter(this.csvFile);
-        CSVWriter writer = new CSVWriter(outputfile);
-        // Creating the default beginning file
-        String[] name = {"Name", username};
-        writer.writeNext(name);
-        String[] win = {"Win", "0"};
-        writer.writeNext(win);
-        String[] loss = {"loss", "0"};
-        writer.writeNext(loss);
-        String[] words = {"words"};
-        writer.writeNext(words);
-        String[] fastestWinTime = {"fastestWinTime", "60"};
-        writer.writeNext(fastestWinTime);
-        // closing writer after it is complete
-        writer.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-    readCsv();
-  }
+		if (!this.csvFile.isFile()) {
+			// create {username}.csv file with default values
+			this.wins = 0;
+			this.losses = 0;
+			this.fastestWinTime = 60;
+			try {
+				rewriteCSV();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		readCsv();
+	}
 
-  /**
-   * reads the .csv file and converts relevant data into the correct data types. Updates the wins,
-   * losses, word history, and fastest win time as per the values in the .csv file
-   *
-   * @throws IOException
-   * @throws CsvException
-   */
-  private void readCsv() throws IOException, CsvException {
-    this.csvList = this.getCsvLines();
-    this.wins = Integer.valueOf(this.csvList.get(1)[1]);
-    this.losses = Integer.valueOf(this.csvList.get(2)[1]);
-    this.wordHistory = Arrays.asList(this.csvList.get(3)).subList(1, this.csvList.get(3).length);
-    this.fastestWinTime = Integer.valueOf(this.csvList.get(4)[1]);
-  }
+	/**
+	 * reads the .csv file and converts relevant data into the correct data types.
+	 * Updates the wins, losses, word history, and fastest win time as per the
+	 * values in the .csv file
+	 *
+	 * @throws IOException
+	 * @throws CsvException
+	 */
+	void readCsv() throws IOException, CsvException {
+		this.csvList = this.getCsvLines();
+		this.wins = Integer.valueOf(this.csvList.get(1)[1]);
+		this.losses = Integer.valueOf(this.csvList.get(2)[1]);
+		this.wordHistory = Arrays.asList(this.csvList.get(3)).subList(1, this.csvList.get(3).length);
+		this.fastestWinTime = Integer.valueOf(this.csvList.get(4)[1]);
+	}
 
-  /**
-   * Reads all lines of a .csv file and returns all the lines as a list of strings
-   *
-   * @return all lines of the csv file as strings
-   * @throws IOException
-   * @throws CsvException
-   */
-  private List<String[]> getCsvLines() throws IOException, CsvException {
-    try (FileReader fr = new FileReader(this.csvFile, StandardCharsets.UTF_8);
-        CSVReader reader = new CSVReader(fr)) {
-      return reader.readAll();
-    }
-  }
+	/**
+	 * Reads all lines of a .csv file and returns all the lines as a list of strings
+	 *
+	 * @return all lines of the csv file as strings
+	 * @throws IOException
+	 * @throws CsvException
+	 */
+	private List<String[]> getCsvLines() throws IOException, CsvException {
+		try (FileReader fr = new FileReader(this.csvFile, StandardCharsets.UTF_8);
+				CSVReader reader = new CSVReader(fr)) {
+			return reader.readAll();
+		}
+	}
 
-  public void incrementWins() {
-    // TODO: Write to CSV file
-    this.wins += 1;
-  }
+	public void rewriteCSV() throws IOException {
+		FileWriter outputfile = new FileWriter(this.csvFile);
+		CSVWriter writer = new CSVWriter(outputfile);
+		// Creating the default beginning file
+		String[] name = { "Name", username };
+		writer.writeNext(name);
+		String[] win = { "Win", this.wins + "" };
+		writer.writeNext(win);
+		String[] loss = { "loss", this.losses + "" };
+		writer.writeNext(loss);
+		// THIS NEEDS TO BE CHANGED FOR THE WORDS LIST
+		String[] words = { "word" };
+		writer.writeNext(words);
+		String[] fastestWinTime = { "fastestWinTime", this.fastestWinTime + "" };
+		writer.writeNext(fastestWinTime);
+		// closing writer after it is complete
+		writer.close();
+	}
 
-  public void incrementLosses() {
-    // TODO: Write to CSV file
-    this.losses += 1;
-  }
+	public void incrementWins() throws IOException {
+		// TODO: Write to CSV file
+		this.wins += 1;
+		rewriteCSV();
+	}
 
-  public void addToHistory(String word) {
-    // TODO: Write to CSV file
-    this.wordHistory.add(word);
-  }
+	public void incrementLosses() throws IOException {
+		// TODO: Write to CSV file
+		this.losses += 1;
+		rewriteCSV();
+	}
 
-  public void setfastestWinTime(int time) {
-    // TODO: Write to CSV file
-    this.fastestWinTime = time;
-  }
+	public void addToHistory(String word) throws IOException {
+		// TODO: Write to CSV file
+		this.wordHistory.add(word);
+	}
 
-  public List<String[]> getCsvList() {
-    return csvList;
-  }
+	public void setfastestWinTime(int time) throws IOException {
+		// TODO: Write to CSV file
+		this.fastestWinTime = time;
+		rewriteCSV();
+	}
 
-  public String getUsername() {
-    return this.username;
-  }
+	public List<String[]> getCsvList() {
+		return csvList;
+	}
 
-  public int getWins() {
-    return this.wins;
-  }
+	public String getUsername() {
+		return this.username;
+	}
 
-  public int getLosses() {
-    return this.losses;
-  }
+	public int getWins() {
+		return this.wins;
+	}
 
-  public List<String> getWordHistory() {
-    return this.wordHistory;
-  }
+	public int getLosses() {
+		return this.losses;
+	}
 
-  public int getfastestWinTime() {
-    return this.fastestWinTime;
-  }
+	public List<String> getWordHistory() {
+		return this.wordHistory;
+	}
+
+	public int getfastestWinTime() {
+		return this.fastestWinTime;
+	}
 }
--- src/test/java/nz/ac/auckland/se206/profiles/ProfileTest.java
+++ src/test/java/nz/ac/auckland/se206/profiles/ProfileTest.java
@@ -2,18 +2,64 @@ package nz.ac.auckland.se206.profiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import com.opencsv.exceptions.CsvException;
 import java.io.IOException;
 import java.net.URISyntaxException;
+
 import org.junit.jupiter.api.Test;
 
-public class ProfileTest {
+import com.opencsv.exceptions.CsvException;
 
-  @Test
-  public void readDefaultCsvTest() throws URISyntaxException, IOException, CsvException {
-    Profile profile = new Profile("test");
-    assertEquals(0, profile.getWins());
-    assertEquals(0, profile.getLosses());
-    assertEquals(60, profile.getfastestWinTime());
-  }
+public class ProfileTest {
+	Profile profile;
+
+	@Test
+	public void readDefaultCsvTest() throws URISyntaxException, IOException, CsvException {
+		Profile readDefault = new Profile("" + System.currentTimeMillis());
+		assertEquals(0, readDefault.getWins());
+		assertEquals(0, readDefault.getLosses());
+		assertEquals(60, readDefault.getfastestWinTime());
+	}
+
+	@Test
+	public void incrementWinsTest() throws IOException, CsvException {
+		Profile winTest = new Profile("" + System.currentTimeMillis());
+		winTest.incrementWins();
+		winTest.readCsv();
+		assertEquals(1, winTest.getWins());
+		winTest.incrementWins();
+		winTest.readCsv();
+		assertEquals(2, winTest.getWins());
+		winTest.incrementWins();
+		winTest.readCsv();
+		assertEquals(3, winTest.getWins());
+	}
+
+	@Test
+	public void incrementLossesTest() throws IOException, CsvException {
+		Profile lossTest = new Profile("" + System.currentTimeMillis());
+		lossTest.incrementLosses();
+		lossTest.readCsv();
+		assertEquals(1, lossTest.getLosses());
+		lossTest.incrementLosses();
+		lossTest.readCsv();
+		assertEquals(2, lossTest.getLosses());
+		lossTest.incrementLosses();
+		lossTest.readCsv();
+		assertEquals(3, lossTest.getLosses());
+	}
+
+	@Test
+	public void changeFastestWinTimeTest() throws IOException, CsvException {
+		Profile fastestTime = new Profile("" + System.currentTimeMillis());
+		fastestTime.setfastestWinTime(50);
+		fastestTime.readCsv();
+		assertEquals(50, fastestTime.getfastestWinTime());
+		fastestTime.setfastestWinTime(25);
+		fastestTime.readCsv();
+		assertEquals(25, fastestTime.getfastestWinTime());
+		fastestTime.setfastestWinTime(20);
+		fastestTime.readCsv();
+		assertEquals(20, fastestTime.getfastestWinTime());
+	}
+
 }
