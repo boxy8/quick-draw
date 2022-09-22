d332df7c25784ed4a60d3c3caf567b589ddbf8c9
MrJonoLloyd <mrjonolloyd@gmail.com>
2022-09-22 13:10:15 +1200

    feat: add selection of profile labels

--- src/main/java/nz/ac/auckland/se206/controllers/ProfileListController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ProfileListController.java
@@ -3,10 +3,18 @@ package nz.ac.auckland.se206.controllers;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.text.TextStringBuilder;
+
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 import nz.ac.auckland.se206.profiles.ProfileLoader;
@@ -25,17 +33,31 @@ public class ProfileListController {
     @FXML
     private Button chooseButton;
 
+    @FXML
+    private VBox profileCards;
+
     public void initialize() {
-        String filePath = "src/main/resources/profiles/";
-        File folder = new File(filePath);
+        String folderPath = "src/main/resources/profiles/";
+        File folder = new File(folderPath);
         File[] listOfFiles = folder.listFiles();
 
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile()) {
-                String username = "File " + listOfFiles[i].getName();
+                String username = listOfFiles[i].getName();
+                Label profileLabel = new Label(username);
+                profileLabel.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
-            } else if (listOfFiles[i].isDirectory()) {
-                System.out.println("Directory " + listOfFiles[i].getName());
+                    public void handle(MouseEvent event) {
+                        Label profileLabel = (Label) event.getSource();
+                        for (Node child : profileCards.getChildren()) {
+                            Label childLabel = (Label) child;
+                            childLabel.setTextFill(Color.BLACK);
+                        }
+
+                        profileLabel.setTextFill(Color.BLUE);
+                    }
+                });
+                profileCards.getChildren().add(profileLabel);
             }
         }
     }
