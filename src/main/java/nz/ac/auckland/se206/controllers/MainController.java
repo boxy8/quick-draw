905101d2baa989ace1a78052f95b7477e32e0f7e
boxy8 <zou.emily@yahoo.co.nz>
2022-09-25 13:40:01 +1300

    feat: make menu bar functional and remove old one

--- src/main/java/nz/ac/auckland/se206/controllers/MainController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MainController.java
@@ -1,6 +1,8 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
@@ -9,6 +11,9 @@ public class MainController {
 
   @FXML private BorderPane border;
 
+  @FXML private Button home;
+
+  // sets the main content (content below the nav bar)
   public void setContent(AppUi appUi) {
     Object controller = SceneManager.getController(appUi);
     if (controller instanceof SwitchListener switchListener) {
@@ -17,5 +22,15 @@ public class MainController {
 
     // switch main content
     border.setCenter(SceneManager.getUiRoot(appUi));
+  }
+
+  @FXML
+  public void onGoHome(ActionEvent event) {
+    SceneManager.changeScene(event, AppUi.MAIN_MENU);
+  }
+
+  @FXML
+  private void onShowProfile(ActionEvent event) {
+    SceneManager.changeScene(event, AppUi.PROFILE_PAGE);
   }
 }
--- src/main/java/nz/ac/auckland/se206/controllers/MainMenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MainMenuController.java
@@ -13,18 +13,11 @@ public class MainMenuController {
 
   @FXML private Label titleLabel;
 
-  @FXML private Button profileButton;
-
   @FXML private Button profilesButton;
 
   @FXML
   private void onPlay(ActionEvent event) {
     SceneManager.changeScene(event, AppUi.CATEGORY_DISPLAY);
-  }
-
-  @FXML
-  private void onShowProfile(ActionEvent event) {
-    SceneManager.changeScene(event, AppUi.PROFILE_PAGE);
   }
 
   @FXML
